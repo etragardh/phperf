51bad694a9e5554ba3a6e609e4d9cb82327310cd
Emil Trägårdh <emil@tstck.io>
2023-12-28 13:22:50 +0100

    update example with hashing comparison

--- example/example.php
+++ example/example.php
@@ -8,6 +8,17 @@ for ( $i=0; $i<=10; $i++ ) {
   echo "Hello ({$i})\r\n";
 }
 $file_contents = file_get_contents("large-file.json");
+unset( $file_contents );
+
+PHPerf::stop();
 
 
+PHPerf::start( 'sha256' );
+$sha256 = hash( "sha256", "test" );
+unset( $sha256 );
+PHPerf::stop();
+
+PHPerf::start( 'md5' );
+$md5 = md5( "test" );
+unset( $md5 );
 PHPerf::stop();
