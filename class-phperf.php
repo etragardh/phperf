cde9aab94353b77d6646c9d31a112e6e6410157c
Emil Trägårdh <emil@tstck.io>
2023-12-28 13:23:18 +0100

    named session support

--- class-phperf.php
+++ class-phperf.php
@@ -17,24 +17,40 @@
 class PHPerf {
 
   private static $start;
+  private static $session_name;
 
   /**
    * Start timer
+   * and resett max memory counter
    */
-  public static function start() {
+  public static function start( $session_name = 'PHPerf Testing' ) {
+    self::$session_name = $session_name;
+    memory_reset_peak_usage();
     self::$start = hrtime( true );
   }
 
   /**
    * Stop timer & print results
+   *
+   * Max Memory does not report max memory between calls as the timer does.
+   * If you want to measure memory make sure nothing else in you php code is peaking.
+   * IE unset( $large_var ); large variables before measuring.
    */
+
+  #################################################################
+  # Session:    PHPerf Testing
+  # Time:       0.032 ms
+  # Max Memory  12 mb
+  #
   public static function stop() {
     $time = hrtime( true );
     $diff = ( $time - self::$start ) / 100000;
-    echo "##############################################\r\n";
-    // Time is displayd in ms with maximum precision
-    echo "# Time\t\t$diff ms\r\n";
-    // Memory is displayed in mb with reasonable precision.
-    echo "# Max Memory\t" . round( memory_get_peak_usage() / 1024 / 1024, 4 ) . " mb\r\n";
+    $mem = round( memory_get_peak_usage() / 1024 / 1024, 4 );
+    $name = self::$session_name;
+
+    echo "##################################################\r\n";
+    echo "# Session:\t\t$name\r\n";                // Name of the testing session 
+    echo "# Time:\t\t\t$diff ms\r\n";                // Time is displayd in ms with maximum precision
+    echo "# Max Memory(*):\t" . $mem . " mb\r\n";  // Memory is displayed in mb with reasonable precision.
   }
 }
